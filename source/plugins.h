f5f5102b33df48f3c6f8347daca385f332c6404b
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:39:49 +0500

    Update plugins.h

--- source/plugins.h
+++ source/plugins.h
@@ -4,11 +4,6 @@
 #include <windows.h>
 #include "list.h"
 
-struct WS_plugins {
-    struct list_head plugins;
-    HMODULE plugin;
-};
-
 void load_plugins(LPCTSTR directory, struct WS_plugins *list);
 
 #endif // PLUGINS_H
