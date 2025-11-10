cbe999e34db212f229122325edcb8641233d8532
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:36:17 +0500

    Update plugins.c

--- source/plugins.c
+++ source/plugins.c
@@ -1,31 +1,40 @@
 #include <windows.h>
-#include <dirent.h>
+#include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 
 #include "plugins.h"
+#include "list.h"
 
 void load_plugins(LPCTSTR directory, struct WS_plugins *list)
 {
-	DIR *dir;
-	struct dirent *ent;
-	struct WS_plugins *t;	
+    DIR *dir;
+    struct dirent *ent;
+    struct WS_plugins *t;
 
-	if ((dir = opendir(directory)) != NULL) 
-	{
-		while((ent = readdir(dir)) != NULL)
-		{
-			char *str = (char*)calloc(MAX_PATH,sizeof(char));
-			strcat(str,directory);
-			strcat(str,ent->d_name);
-			HMODULE h = LoadLibrary(str);
-			if(h != NULL)
-			{
-				t = (struct WS_plugins*)malloc(sizeof(struct WS_plugins));
-				t->plugin = h;
-				list_add(&(t->plugins),&(list->plugins));
-			}
-		}
-		closedir (dir);
-	}
-	return;
+    if ((dir = opendir(directory)) != NULL)
+    {
+        while ((ent = readdir(dir)) != NULL)
+        {
+            if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0)
+                continue;
+
+            char fullpath[MAX_PATH];
+            snprintf(fullpath, sizeof(fullpath), "%s/%s", directory, ent->d_name);
+
+            HMODULE h = LoadLibraryA(fullpath);
+            if (h != NULL)
+            {
+                t = (struct WS_plugins *)malloc(sizeof(struct WS_plugins));
+                if (t)
+                {
+                    t->plugin = h;
+                    INIT_LIST_HEAD(&t->plugins);
+                    list_add(&(t->plugins), &(list->plugins));
+                }
+            }
+        }
+        closedir(dir);
+    }
 }
