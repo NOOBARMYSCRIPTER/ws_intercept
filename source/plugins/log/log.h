a71620225a21d41ad496af5dfcf627d1c7823270
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:33:34 +0500

    Update log.h

--- source/plugins/log/log.h
+++ source/plugins/log/log.h
@@ -2,22 +2,20 @@
 #define LOG_H
 
 #include <stdio.h>
+#include <stdint.h>
+#include <time.h>
 #include "../../ws.h"
 
-#define LOGGING 1 //TODO: Just add a makefile option for this
+#define LOGGING 1
 
-#ifndef LOGGING
-
+#if LOGGING == 0
 #define LOG(x,...) do { printf(x, ##__VA_ARGS__); printf("\n"); } while(0)
-#define LOGn(x,...) do { printf(x, ##__VA_ARGS__); } while(0) //Log without newline
+#define LOGn(x,...) do { printf(x, ##__VA_ARGS__); } while(0)
 
 #elif LOGGING == 1
-
-#include <time.h>
 FILE *logfile = NULL;
-
 #define LOG(x,y,z) do { fwrite(x,y,z,logfile); fflush(logfile); } while(0)
-
+#define LOGn(x,...) do { } while(0)
 #endif
 
 #endif //LOG_H
