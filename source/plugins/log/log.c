efc4547fc18206108f11a48693743ca183aa7b5a
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:33:23 +0500

    Update log.c

--- source/plugins/log/log.c
+++ source/plugins/log/log.c
@@ -1,5 +1,8 @@
 #include <winsock2.h>
 #include <windows.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
 #include "log.h"
 
 static DWORD WINAPI setup_console(LPVOID param);
@@ -8,7 +11,6 @@ static DWORD WINAPI console_handler(LPVOID param);
 void WINAPI log_ws(SOCKET *s, const char *buf, int *len, int *flags);
 
 static DWORD threadIDConsole = 0;
-
 static DWORD plugin_id_send = 0;
 static DWORD plugin_id_recv = 0;
 
@@ -25,34 +27,31 @@ BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
 			if (threadIDConsole)
 				PostThreadMessage(threadIDConsole, WM_QUIT, 0, 0);
 			break;
-		case DLL_THREAD_ATTACH:
-			break;
-		case DLL_THREAD_DETACH:
-			break;
+		case DLL_THREAD_ATTACH: break;
+		case DLL_THREAD_DETACH: break;
 	}
 	return TRUE;
 }
 
-
-void WINAPI log_ws(SOCKET *s, const char *buf, int *len, int *flags) //Note that you're given pointers to everything! (buf was already a pointer though)
+void WINAPI log_ws(SOCKET *s, const char *buf, int *len, int *flags)
 {
 	struct sockaddr_in info;
-	int infolen;
-	getpeername(*s,(struct sockaddr*)(&info),&infolen);
-	const short port = ntohs(info.sin_port);
+	socklen_t infolen = sizeof(info);
+	getpeername(*s, (struct sockaddr*)&info, &infolen);
+	const uint16_t port = ntohs(info.sin_port);
+
 #if LOGGING == 1
-	//<SOCKET:4><ADDR:4><PORT:2><LEN:4><FLAGS:4><DATA:LEN>
-	LOG(s,sizeof(SOCKET),1);
-	LOG(&info.sin_addr,sizeof(struct in_addr),1);
-	LOG(&port,sizeof(short),1);
-	LOG(len,sizeof(int),1);
-	LOG(flags,sizeof(int),1);
-	LOG(buf,sizeof(char),*len); //If for whatever reason len != buffer size, then there's some bigger underlying problem... or another plugin is messing with something
-#else	
-	LOG("%s:%u, Len %d, Flags %d, socket %u",inet_ntoa(info.sin_addr), port, *len, *flags, *s);
+	LOG(s, sizeof(SOCKET), 1);
+	LOG(&info.sin_addr, sizeof(struct in_addr), 1);
+	LOG(&port, sizeof(uint16_t), 1);
+	LOG(len, sizeof(int), 1);
+	LOG(flags, sizeof(int), 1);
+	LOG(buf, sizeof(char), *len);
+#else
+	LOG("%s:%u, Len %d, Flags %d, socket %llu", inet_ntoa(info.sin_addr), port, *len, *flags, (unsigned long long)*s);
 	LOGn("Data: ");  
 	for(int i = 0; i < *len; i++) 
-		LOGn("%02X ",(unsigned char)buf[i]);
+		LOGn("%02X ", (unsigned char)buf[i]);
 	LOGn("\n");
 #endif
 	return;
@@ -91,12 +90,12 @@ static inline void toggle_recv()
 
 static DWORD WINAPI console_handler(LPVOID param)
 {
-	int choice;
+	int choice = 0;
 	while(1)
 	{
 		help_text();
 		printf("Current value: %d", choice);
-		scanf("%d",&choice);
+		scanf("%d", &choice);
 		if(!choice)
 		{
 			unregister_handler(plugin_id_send, WS_HANDLER_SEND);
@@ -105,10 +104,8 @@ static DWORD WINAPI console_handler(LPVOID param)
 			plugin_id_recv = 0;
 			continue;
 		}
-		if(choice & 0x1)
-			toggle_send();
-		if(choice & 0x2)
-			toggle_recv();
+		if(choice & 0x1) toggle_send();
+		if(choice & 0x2) toggle_recv();
 	}
 	return 0;
 }
@@ -116,10 +113,9 @@ static DWORD WINAPI console_handler(LPVOID param)
 static DWORD WINAPI setup_console(LPVOID param)
 {
 #if LOGGING == 1
-	char *name = malloc(sizeof(char)*30);
-	sprintf(name,"log_%u.bin",(unsigned int)time(NULL));
+	char name[64];
+	sprintf(name,"log_%llu.bin",(unsigned long long)time(NULL));
 	logfile = fopen(name,"wb");
-	free(name);
 #endif
 	AllocConsole();
 	freopen("CONOUT$","w",stdout);
@@ -130,11 +126,13 @@ static DWORD WINAPI setup_console(LPVOID param)
 		MSG msg;
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) 
 		{
-			switch (msg.message) 
+			if(msg.message == WM_QUIT)
 			{
-				case WM_QUIT:
-					FreeConsole();		
-					return msg.wParam;
+				FreeConsole();		
+#if LOGGING == 1
+				fclose(logfile);
+#endif
+				return msg.wParam;
 			}
 		}
 	}
