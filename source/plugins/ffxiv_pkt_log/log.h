bace31c16d3483cff746eb20d9b4a822609a7985
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:28:23 +0500

    Update log.h

--- source/plugins/ffxiv_pkt_log/log.h
+++ source/plugins/ffxiv_pkt_log/log.h
@@ -2,9 +2,10 @@
 #define LOG_H
 
 #include <stdio.h>
+#include <stdint.h>
 #include "../../ws.h"
 
 #define LOG(x,...) do { __mingw_printf(x, ##__VA_ARGS__); __mingw_printf("\n"); } while(0)
 #define LOGn(x,...) do { __mingw_printf(x, ##__VA_ARGS__); } while(0) //Log without newline
 
-#endif //LOG_H
+#endif // LOG_H
