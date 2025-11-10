67c046bd400ef2efffa0aa0821e2f9a0673f4195
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 19:30:21 +0500

    Update ws.c

--- source/ws.c
+++ source/ws.c
@@ -1,14 +1,11 @@
 #include <winsock2.h>
 #include <windows.h>
-#include <psapi.h>
 #include <stdio.h>
-
+#include "MinHook.h"
 #include "ws.h"
 #include "misc.h"
 #include "plugins.h"
 #include "list.h"
-
-#define MAX_PACKET 4096
 
 struct WS_plugins ws_plugins;
 struct WS_handler ws_handlers;
@@ -16,21 +13,11 @@ struct WS_handler ws_handlers;
 typedef int (WINAPI *tWS_send)(SOCKET, const char*, int, int);
 typedef int (WINAPI *tWS_recv)(SOCKET, char*, int, int);
 
-static DWORD WINAPI initialize(LPVOID param);
-static void revert();
+static tWS_send origSend = NULL;
+static tWS_recv  origRecv = NULL;
 
-static int WINAPI repl_recv(SOCKET s, char *buf, int len, int flags);
 static int WINAPI repl_send(SOCKET s, const char *buf, int len, int flags);
-
-static tWS_send pSend = NULL;
-static tWS_recv  pRecv = NULL;
-
-static BYTE replaced_send[10];
-static BYTE replaced_recv[10];
-static uintptr_t orig_size_send = 0;
-static uintptr_t orig_size_recv = 0;
-static uintptr_t addr_send = 0;
-static uintptr_t addr_recv = 0;
+static int WINAPI repl_recv(SOCKET s, char *buf, int len, int flags);
 
 LIBAPI uintptr_t register_handler(tWS_plugin func, WS_HANDLER_TYPE type, char *comment)
 {
@@ -39,10 +26,12 @@ LIBAPI uintptr_t register_handler(tWS_plugin func, WS_HANDLER_TYPE type, char *c
     t->func = func;
     t->comment = (char*)malloc(strlen(comment)+1);
     strcpy(t->comment, comment);
+
     if(type & WS_HANDLER_SEND)
         list_add_tail(&(t->ws_handlers_send), &(ws_handlers.ws_handlers_send));
     else
         list_add_tail(&(t->ws_handlers_recv), &(ws_handlers.ws_handlers_recv));
+
     return (uintptr_t)t;
 }
 
@@ -55,15 +44,35 @@ LIBAPI void unregister_handler(uintptr_t plugin_id, WS_HANDLER_TYPE type)
         list_del(&((struct WS_handler*)plugin_id)->ws_handlers_recv);
 }
 
+static DWORD WINAPI initialize(LPVOID param)
+{
+    if(MH_Initialize() != MH_OK) return 1;
+
+    if(MH_CreateHookApi(L"Ws2_32.dll", "send", repl_send, (LPVOID*)&origSend) != MH_OK) return 1;
+    if(MH_EnableHook(MH_ALL_HOOKS) != MH_OK) return 1;
+
+    if(MH_CreateHookApi(L"Ws2_32.dll", "recv", repl_recv, (LPVOID*)&origRecv) != MH_OK) return 1;
+    if(MH_EnableHook(MH_ALL_HOOKS) != MH_OK) return 1;
+
+    INIT_LIST_HEAD(&ws_handlers.ws_handlers_send);
+    INIT_LIST_HEAD(&ws_handlers.ws_handlers_recv);
+    INIT_LIST_HEAD(&ws_plugins.plugins);
+
+    load_plugins("./plugins/", &ws_plugins);
+
+    return 0;
+}
+
 BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
 {
     switch(reason)
     {
         case DLL_PROCESS_ATTACH:
-            CreateThread(NULL,0,initialize,NULL,0,NULL);
+            CreateThread(NULL, 0, initialize, NULL, 0, NULL);
             break;
         case DLL_PROCESS_DETACH:
-            revert();
+            MH_DisableHook(MH_ALL_HOOKS);
+            MH_Uninitialize();
             list_for_each(t, &ws_plugins.plugins)
                 FreeLibrary(list_entry(t, struct WS_plugins, plugins)->plugin);
             break;
@@ -71,68 +80,6 @@ BOOL APIENTRY DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
             break;
     }
     return TRUE;
-}
-
-static DWORD WINAPI initialize(LPVOID param)
-{
-    uintptr_t addr;
-    BYTE replaced[10];
-    uintptr_t orig_size;
-    char cwdBuf[MAX_PATH], tmpPath[MAX_PATH], logPath[MAX_PATH];
-
-    if(GetTempPathA(MAX_PATH, tmpPath)==0)
-        strncpy(tmpPath, ".\\", MAX_PATH);
-
-    if(tmpPath[strlen(tmpPath)-1]!='\\') strncat(tmpPath,"\\",MAX_PATH-strlen(tmpPath)-1);
-
-    snprintf(logPath, MAX_PATH, "%sws_init_log.txt", tmpPath);
-    FILE *dbg = fopen(logPath,"a");
-    if(dbg) fprintf(dbg,"initialize start pid=%lu\n",(unsigned long)GetCurrentProcessId());
-
-    addr_send = (uintptr_t)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")),"send");
-    addr_recv = (uintptr_t)GetProcAddress(GetModuleHandle(TEXT("WS2_32.dll")),"recv");
-
-    if(dbg){
-        if(addr_send) fprintf(dbg,"addr_send=0x%p\n",(void*)addr_send);
-        if(addr_recv) fprintf(dbg,"addr_recv=0x%p\n",(void*)addr_recv);
-    }
-
-    // hook send
-    addr = addr_send;
-    if(apply_patch(0xE9, (SIZE_T)addr, (void*)&repl_send, (SIZE_T*)&orig_size_send, replaced_send)){
-        pSend = (tWS_send)VirtualAlloc(NULL, orig_size_send+32, MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        if(pSend){
-            memcpy(pSend,replaced_send,orig_size_send);
-            apply_patch(0xE9,(SIZE_T)((BYTE*)pSend+orig_size_send),(void*)(addr+orig_size_send),(SIZE_T*)&orig_size,replaced);
-        }else if(dbg) fprintf(dbg,"VirtualAlloc pSend failed, err=%lu\n",GetLastError());
-    }else if(dbg) fprintf(dbg,"apply_patch send failed\n");
-
-    // hook recv
-    addr = addr_recv;
-    if(apply_patch(0xE9,(SIZE_T)addr,(void*)&repl_recv,(SIZE_T*)&orig_size_recv,replaced_recv)){
-        pRecv = (tWS_recv)VirtualAlloc(NULL, orig_size_recv+32, MEM_RESERVE|MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-        if(pRecv){
-            memcpy(pRecv,replaced_recv,orig_size_recv);
-            apply_patch(0xE9,(SIZE_T)((BYTE*)pRecv+orig_size_recv),(void*)(addr+orig_size_recv),(SIZE_T*)&orig_size,replaced);
-        }else if(dbg) fprintf(dbg,"VirtualAlloc pRecv failed, err=%lu\n",GetLastError());
-    }else if(dbg) fprintf(dbg,"apply_patch recv failed\n");
-
-    INIT_LIST_HEAD(&ws_handlers.ws_handlers_send);
-    INIT_LIST_HEAD(&ws_handlers.ws_handlers_recv);
-    INIT_LIST_HEAD(&ws_plugins.plugins);
-
-    if(GetCurrentDirectoryA(MAX_PATH,cwdBuf)>0 && dbg)
-        fprintf(dbg,"cwd=%s\n",cwdBuf);
-
-    if(dbg){ fflush(dbg); fclose(dbg); }
-    load_plugins("./plugins/", &ws_plugins);
-    return 0;
-}
-
-static void revert()
-{
-    if(addr_send) exec_copy((SIZE_T)addr_send, replaced_send, orig_size_send);
-    if(addr_recv) exec_copy((SIZE_T)addr_recv, replaced_recv, orig_size_recv);
 }
 
 static int WINAPI repl_send(SOCKET s, const char *buf, int len, int flags)
@@ -143,8 +90,9 @@ static int WINAPI repl_send(SOCKET s, const char *buf, int len, int flags)
         if(handler && handler->func)
             handler->func(&s, buf, &len, &flags);
     }
-    if(!pSend) return SOCKET_ERROR;
-    return pSend(s, buf, len, flags);
+
+    if(!origSend) return SOCKET_ERROR;
+    return origSend(s, buf, len, flags);
 }
 
 static int WINAPI repl_recv(SOCKET s, char *buf, int len, int flags)
@@ -155,6 +103,7 @@ static int WINAPI repl_recv(SOCKET s, char *buf, int len, int flags)
         if(handler && handler->func)
             handler->func(&s, buf, &len, &flags);
     }
-    if(!pRecv) return SOCKET_ERROR;
-    return pRecv(s, buf, len, flags);
+
+    if(!origRecv) return SOCKET_ERROR;
+    return origRecv(s, buf, len, flags);
 }
