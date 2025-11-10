d5dc8f83121f0ccc084dea294ea34b662096e1ff
Okho <109078241+NOOBARMYSCRIPTER@users.noreply.github.com>
2025-11-10 18:24:24 +0500

    Update misc.c

--- source/misc.c
+++ source/misc.c
@@ -1,36 +1,35 @@
 #include <windows.h>
 #include "misc.h"
 
-//***************************
-//http://stackoverflow.com/questions/13026220/c-function-hook-memory-address-only
 #pragma pack(1)
 struct patch_t
 {
-    BYTE nPatchType; //OP code, 0xE9 for JMP
-    DWORD dwAddress;
+    BYTE nPatchType;
+    SIZE_T dwAddress;
 };
 #pragma pack()
 
-BOOL apply_patch(BYTE eType, DWORD dwAddress, const void *pTarget, DWORD *orig_size, BYTE *replaced)
+BOOL apply_patch(BYTE eType, SIZE_T dwAddress, const void *pTarget, SIZE_T *orig_size, BYTE *replaced)
 {
-	DWORD dwOldValue, dwTemp;
-	struct patch_t pWrite =
-	{
-		eType,
-		(DWORD)pTarget - (dwAddress + sizeof(DWORD) + sizeof(BYTE))
-	};
-	VirtualProtect((LPVOID)dwAddress,sizeof(DWORD),PAGE_EXECUTE_READWRITE,&dwOldValue);
-	ReadProcessMemory(GetCurrentProcess(),(LPVOID)dwAddress,(LPVOID)replaced,sizeof(pWrite),(PDWORD)orig_size); //Keep track of the bytes we replaced
-	BOOL bSuccess = WriteProcessMemory(GetCurrentProcess(),(LPVOID)dwAddress,&pWrite,sizeof(pWrite),NULL);
-	VirtualProtect((LPVOID)dwAddress,sizeof(DWORD),dwOldValue,&dwTemp);
-	
+    SIZE_T dwOldValue, dwTemp;
+    struct patch_t pWrite =
+    {
+        eType,
+        (SIZE_T)pTarget - (dwAddress + sizeof(SIZE_T) + sizeof(BYTE))
+    };
+
+    VirtualProtect((LPVOID)dwAddress, sizeof(struct patch_t), PAGE_EXECUTE_READWRITE, (PDWORD)&dwOldValue);
+    ReadProcessMemory(GetCurrentProcess(), (LPCVOID)dwAddress, (LPVOID)replaced, sizeof(pWrite), orig_size);
+    BOOL bSuccess = WriteProcessMemory(GetCurrentProcess(), (LPVOID)dwAddress, &pWrite, sizeof(pWrite), NULL);
+    VirtualProtect((LPVOID)dwAddress, sizeof(struct patch_t), dwOldValue, &dwTemp);
+
     return bSuccess;
 }
 
-inline void exec_copy(DWORD addr, BYTE *replaced, DWORD orig_size)
+inline void exec_copy(SIZE_T addr, BYTE *replaced, SIZE_T orig_size)
 {
-		DWORD old_val, temp;
-		VirtualProtect((LPVOID)addr,sizeof(DWORD),PAGE_EXECUTE_READWRITE,&old_val);
-		memcpy((void*)addr,replaced,orig_size);
-		VirtualProtect((LPVOID)addr,sizeof(DWORD),old_val,&temp);
+    SIZE_T old_val, temp;
+    VirtualProtect((LPVOID)addr, orig_size, PAGE_EXECUTE_READWRITE, (PDWORD)&old_val);
+    memcpy((void*)addr, replaced, orig_size);
+    VirtualProtect((LPVOID)addr, orig_size, old_val, &temp);
 }
